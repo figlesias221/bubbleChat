1bceb2cd3c092bef5d2880bbf58cf3c64d5c8f73
Federico Iglesias <figlesias221@gmail.com>
2023-06-15 18:52:20 -0300

    Update

--- bubbleChat.js
+++ bubbleChat.js
@@ -1,4 +1,4 @@
-const SIZE = 100; // size of the chat button in pixels
+const SIZE = 20; // size of the chat button in pixels
 const BTN_RAD = SIZE / 2; // radius of the chat button in pixels
 const BG_CHAT = "#49CACD"; // background color of the chat button
 const chatButtonLogo = `<svg fill="#FFFFFF" stroke-width="10" height="60px" width="700px" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" 
@@ -97,7 +97,6 @@ overlay.addEventListener("click", () => {
   chatButtonIcon.innerHTML = chatButtonLogo;
   overlay.style.display = "none";
 });
-
 
 // Responsive
 function adjustForSmallScreens() {
